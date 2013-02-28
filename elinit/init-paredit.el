dcf192809aa1b13f1263c0d452b74f0db4b94abe
Stefan Oestreicher <oestef@gmail.com>
2013-02-28 14:15:14 +0100

    hook paredit into lisp, scheme and clojure modes

---
+++ elinit/init-paredit.el
@@ -0,0 +1,4 @@
+(add-hook 'clojure-mode-hook (lambda () (paredit-mode 1)))
+(add-hook 'scheme-mode-hook (lambda () (paredit-mode 1)))
+(add-hook 'lisp-mode-hook (lambda () (paredit-mode 1)))
+(add-hook 'emacs-lisp-mode-hook (lambda () (paredit-mode 1)))
